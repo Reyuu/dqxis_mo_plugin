d7622c280db725a73ef02dfff8251ed23c29e6dc
Reyuu <7038406+Reyuu@users.noreply.github.com>
2025-02-04 20:23:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is an unofficial plugin for **DRAGON QUEST XI S: Echoes of an Elusive Age - Definitive Edition**.
+
+## Installation
+To install simply download the repo and unpack it to `<Mod Organizer 2 Directory>/plugins/basic_games/games`.
+
+## Uninstallation
+Remove the file from `<Mod Organizer 2 Directory>/plugins/basic_games/games`.
---
+++ game_dqxis.py
@@ -0,0 +1,18 @@
+from PyQt6.QtCore import QFileInfo
+
+from ..basic_game import BasicGame
+
+
+class DragonQuestXIS(BasicGame):
+    Name = "DRAGON QUEST XI S: Echoes of an Elusive Age - Definitive Edition Plugin"
+    Author = "Reyuu"
+    Version = "1.0.0"
+
+    GameName = "DRAGON QUEST XI S: Echoes of an Elusive Age - Definitive Edition"
+    GameShortName = "dqxis"
+    GameNexusName = "dragonquestxisdefinitiveedition"
+    GameNexusId = 3496
+    GameEpicId = "94a50905f377470c84c8e4b7c38f9cdd"
+    GameSteamId = 1295510
+    GameBinary = "Game/Binaries/Win64/DRAGON QUEST XI S.exe"
+    GameDataPath = "Game/Content"
